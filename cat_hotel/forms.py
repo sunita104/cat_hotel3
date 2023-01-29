7168650551c2114c614131fcd95ca5820ec54f14
sunita.bo <sunita.bo.63@ubu.ac.th>
2023-01-30 04:19:16 +0700

    first commit

--- cat_hotel/forms.py
+++ cat_hotel/forms.py
@@ -3,8 +3,7 @@ from .models import Booking, Room, Customer
 
 class BookingForm(forms.ModelForm):
     room = forms.ModelChoiceField(queryset=Room.objects.all())
-    customer = forms.ModelChoiceField(queryset=Customer.objects.all())
-    start_date = forms.DateField()
+    start_date = forms.DateField(widget=forms.HiddenInput())
     start_time = forms.TimeField()
     end_date = forms.DateField()
     end_time = forms.TimeField()
@@ -13,4 +12,4 @@ class BookingForm(forms.ModelForm):
 
     class Meta:
         model = Booking
-        fields = ['customer', 'room', 'start_date', 'start_time', 'end_date', 'end_time', 'cat', 'phone_number']
+        fields = [ 'room', 'start_date', 'start_time', 'end_date', 'end_time', 'cat', 'phone_number']
--- cat_hotel/views.py
+++ cat_hotel/views.py
@@ -27,23 +27,29 @@ def cat_hotels(requset):
 
 
 def cat_hotel(request, number_room):
-    one_room = None
-    try:
-        one_room = Room.objects.get(number_room=number_room)
-    except Room.DoesNotExist:
-        print('Room not found')
-        raise Http404("Room not found")
-
+    one_room = Room.objects.get(number_room=number_room)
     if request.method == 'POST':
-        form = BookingForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('success')
-    else:
-        form = BookingForm(initial={'room': one_room})
-        
-    context = { 'cat_hotel': one_room, 'form': form }
+        start_date = request.POST.get('start_date')
+        start_time = request.POST.get('start_time')
+        end_date = request.POST.get('end_date')
+        end_time = request.POST.get('end_time')
+        cat = request.POST.get('cat')
+        phone_number = request.POST.get('phone_number')
+
+        obj, created = CatHotel.objects.get_or_create(
+            room=one_room,
+            start_date=start_date,
+            start_time=start_time,
+            end_date=end_date,
+            end_time=end_time,
+            cat=cat,
+            phone_number=phone_number
+        )
+        obj.save()
+        return redirect('success')
+    context = {'cat_hotel': one_room}
     return render(request, 'cat_hotel/cat_hotel.html', context)
+    
 
 
 def success(request):
